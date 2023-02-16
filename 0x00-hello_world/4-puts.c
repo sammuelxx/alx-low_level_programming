c1c601296fb5d155494287a2de55605d1f5761dd
Samm <opeyemi6819@gmail.com>
2023-02-16 07:51:54 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a c program that prints a line with puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
