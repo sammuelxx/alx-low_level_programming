ae585917ed981d4614f5ac47933703009c418737
Samm <opeyemi6819@gmail.com>
2023-02-17 11:30:31 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int s = 0, e = 7, s2 = 1, e2 = 8, s3 = 2, e3 = 9;
+
+	while (s <= e)
+	{
+		s2 = s + 1;
+		while (s2 <= e2)
+		{
+			s3 = s2 + 1;
+			while (s3 <= e3)
+			{
+				putchar(s + '0');
+				putchar(s2 + '0');
+				putchar(s3 + '0');
+
+				if (s != e)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				s3++;
+			}
+			s2++;
+		}
+		s++;
+	}
+	putchar('\n');
+
+	return (0);
+}
