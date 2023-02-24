230ee67f7e8ffec1bbee48c3bf27220c466eb6d9
Samm <opeyemi6819@gmail.com>
2023-02-24 04:40:34 -0800

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,12 +15,12 @@ void print_triangle(int size)
 
 		for (i = 1; i <= size; i++)
 		{
-			for (i = i; j < size; j++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
 
-			for (i = 1; j <= i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
