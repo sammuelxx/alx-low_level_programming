1418abc1bc4af085d0b6a948c2d2e7f85beca1e2
Samm <opeyemi6819@gmail.com>
2023-03-06 09:13:30 -0800

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Emtry point
+ * @s: Input
+ * @c: Input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int 1 = 0;
+
+	for (; s[i] >= '\a'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
